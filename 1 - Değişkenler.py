e45f91daa6acf21878ad3ae1e77f84a721748319
Yiğit <109026201+Frowy@users.noreply.github.com>
2024-06-03 20:17:47 +0300

    Add files via upload
    Python'da değişkenler ile alakalı örnekler

---
+++ 1 - Değişkenler.py
@@ -0,0 +1,29 @@
+a = 1 # int tipi değer
+b = "Bir" # str tipi değer
+print(a) # Değişkeni yazdırır
+print(type(a)) # Değişkenin türünü "type" ile yazdırırız
+
+#? Değişkenleri "" ile veya '' atayabiliriz
+#? Değişken isimlerinde büyük harf hassasiyeti vardır
+
+c, d, e = "Ceyhan", "Denizli", "Edirne"
+print(c) # Ceyhan
+print(d) # Denizli
+print(e) # Edirne
+
+C = D =  E = "Hepsi aynı değer"
+print(C) # Hepsi aynı değer
+print(D) # Hepsi aynı değer
+print(E) # Hepsi aynı değer
+
+#  Matematiksel İşlemler
+A = 2
+B = 3
+print(A + B) # Toplama
+print(A - B) # Çıkarma
+print(A * B) # Çarpma
+print(A / B) # Bölme
+
+hl = "Merhaba Dünya!"
+print(len(hl)) # Cümlenin uzunluğu
+print("Merhaba" in hl) # Cümlenin içinde varmı
